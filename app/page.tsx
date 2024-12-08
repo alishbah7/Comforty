f37880eb41eea728b949eaff3f91330927b8abcf
alisbah7 <alishbah04ak@gmail.com>
2024-12-08 05:19:27 +0500

    Comforty

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,45 @@
+import BrandsSlider from "@/components/brands";
+import FeaturedProducts from "@/components/featured";
 import Header from "@/components/header";
 import Hero from "@/components/hero";
 // import Image from "next/image";
-
+const products = [  
+  {  
+    title: "Library Stool Chair",  
+    price: 20,  
+    salePrice: null,  
+    badge: 'New',  
+    image: "/images/fp1.png",
+  },  
+  {  
+    title: "Library Stool Chair",  
+    price: 20,  
+    salePrice: 30,  
+    badge: 'Sales',  
+    image: "/images/fp2.png",  
+  },  
+  {  
+    title: "Library Stool Chair",  
+    price: 20,  
+    salePrice: null,  
+    badge: null,  
+    image: "/images/fp3.png",  
+  },  
+  {  
+    title: "Library Stool Chair",  
+    price: 20,  
+    salePrice: null,  
+    badge: null,  
+    image: "/images/fp4.png",  
+  },  
+]; 
 export default function Home() {
   return (
     <div>
       <Header/>
       <Hero/>
+      <BrandsSlider/>
+      <FeaturedProducts products={products}/>
     </div>
   );
 }
---
+++ components/featured.tsx
@@ -0,0 +1,52 @@
+import React from 'react';  
+import Image from 'next/image';
+
+interface Product  {  
+    title: string;  
+    price: number;  
+    salePrice: number | null;  
+    badge: "New" | "Sales" | null;  
+    image: string;  
+  };  
+
+interface FeaturedProductsProps {  
+  products: Product[];  
+}  
+
+const FeaturedProducts: React.FC<FeaturedProductsProps> = ({ products }) => {  
+  return (  
+    <div className="container mx-auto my-10 px-5">  
+      <h2 className="flex justify-center items-center text-center sm:text-start sm:block text-2xl font-bold text-gray-800">Featured Products</h2>  
+      <div className="flex flex-col justify-center items-center sm:grid sm:grid-cols-2 md:grid-cols-4 gap-6 mt-5">  
+        {products.map((product, index) => (  
+          <div key={index} className="overflow-hidden">  
+            <div className="relative">  
+              {product.badge && (  
+                <span className={`absolute top-2 left-2 text-white text-xs py-[5px] px-[8px] w-[45px] rounded-md ${product.badge === 'New' ? 'bg-green-500' : 'bg-orange-500'}`}>  
+                  {product.badge}  
+                </span>  
+              )}  
+              <Image src={product.image} alt={product.title} className="" width={300} height={300}/>  
+            </div>  
+            <div className="flex justify-between p-[10px]">  
+                <div>
+                    <h3 className="font-light text-lg tracking-wide text-gray-700">{product.title}</h3>  
+                    <div className='flex gap-[10px]'>
+                    {product.salePrice && <span className="text-black"> ${product.price}</span>} 
+                    <p className={`text-black ${product.salePrice ? 'line-through' : ''}`}>  
+                        ${product.salePrice ? product.salePrice : product.price}  
+                    </p>  
+                    </div>
+              </div>
+              <div>
+                <button className="cartBg text-white px-[10px] py-[5px] rounded-md text-[20px]"><i className='bx bx-cart-alt'></i></button>  
+              </div>
+            </div>  
+          </div>  
+        ))}  
+      </div>  
+    </div>  
+  );  
+};  
+
+export default FeaturedProducts;
