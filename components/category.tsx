397167a04a53498f3a9985d48a4a03306cfb4dbe
alisbah7 <alishbah04ak@gmail.com>
2024-12-09 01:38:06 +0500

    Comforty

--- components/category.tsx
+++ components/category.tsx
@@ -21,8 +21,8 @@ const CategoryCard: React.FC<CategoryCardProps> = ({ image, title, productCount 
     AOS.init({ duration: 2000, easing: 'ease', delay: 200 });
 }, []);
   return (  
-    <div className="relative rounded-lg overflow-hidden shadow-lg w-[250px] md:w-[380px]">  
-      <Image src={image} alt={title} className="w-[250px] md:w-[380px] h-auto" width={200} height={200}/>  
+    <div className="relative rounded-lg overflow-hidden shadow-lg w-[250px] md:w-[350px]">  
+      <Image src={image} alt={title} className="w-[250px] md:w-[350px] h-auto" width={200} height={200}/>  
       <div className="absolute bottom-0 left-0 right-0 bg-black bg-opacity-60 text-white text-center p-4">  
         <h3 className="text-lg font-semibold tracking-wide text-start">{title}</h3>  
         <p className="text-sm text-start text-gray-300">{productCount} Products</p>  
@@ -38,7 +38,7 @@ interface CategorySectionProps {
 const TopCategory: React.FC<CategorySectionProps> = ({ categories }) => {  
   return (  
     <div className="flex flex-col justify-center items-center p-8" >  
-      <h2 className="flex justify-center items-center md:justify-start md:items-start text-xl font-bold mb-4 w-[100%] xl:w-[90%] text-start" data-aos='fade-right'>Top Categories</h2>  
+      <h2 className="flex justify-center gap-[20px] items-center md:justify-start md:items-start text-xl font-bold mb-4 w-[100%] xl:w-[90%] text-start" data-aos='fade-right'>Top Categories</h2>  
       <div data-aos='zoom-in' className="flex flex-col justify-center items-center xl:grid xl:grid-cols-3 gap-[20px] xl:gap-4 w-[100%] lg:w-[90%]">  
         {categories.map((category) => (  
           <CategoryCard  
